5a49b685c40db00164db6fa2bce6d7c120a0b636
Alexandre Bagio da Luz <alexandre.luz@satc.edu.br>
2016-08-04 15:00:16 -0300

    master parte 2

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,20 +3,21 @@ namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Request;
+use estoque\Produto;
 
 class ProdutoController extends Controller
 {
     public function lista()
     {
-        $produtos = DB::select('select * from produtos');
+        $produtos = Produto::all();
         return view('produtos.listagem')->with('produtos', $produtos);
     }
     
     public function mostra()
     {
         $id = Request::route('id');
-        $produtos = DB::select('select * from produtos where id = ?', [$id]);
-        return view('produtos.detalhes')->with('p', $produtos[0]);
+        $produtos = Produto::find($id);
+        return view('produtos.detalhes')->with('p', $produtos);
     }
 
     public function novo()
@@ -26,14 +27,12 @@ class ProdutoController extends Controller
 
     public function adiciona()
     {
-        $nome = Request::input('nome');
-        $valor = Request::input('valor');
-        $quantidade = Request::input('quantidade');
-        $descricao = Request::input('descricao');
-        DB::insert(
-            'insert into produtos (nome, valor, quantidade, descricao) values(?,?,?,?)',
-            array($nome, $valor, $quantidade, $descricao)
-        );
+        $produto = new Produto();
+        $produto->nome = Request::input('nome');
+        $produto->valor = Request::input('valor');
+        $produto->quantidade = Request::input('quantidade');
+        $produto->descricao = Request::input('descricao');
+        $produto->save();
         //return view('produtos.adicionado')->with('nome', $nome);
         return redirect('/produtos')->withInput();
     }
