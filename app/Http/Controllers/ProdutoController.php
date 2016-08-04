af01cacbe6f7892ca791243d2216acfe47c630a4
Alexandre Bagio da Luz <alexandre.luz@satc.edu.br>
2016-08-04 13:00:57 -0300

    Finalizando primeira etapa

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -18,4 +18,23 @@ class ProdutoController extends Controller
         $produtos = DB::select('select * from produtos where id = ?', [$id]);
         return view('produtos.detalhes')->with('p', $produtos[0]);
     }
+
+    public function novo()
+    {
+        return view('produtos.formulario');
+    }
+
+    public function adiciona()
+    {
+        $nome = Request::input('nome');
+        $valor = Request::input('valor');
+        $quantidade = Request::input('quantidade');
+        $descricao = Request::input('descricao');
+        DB::insert(
+            'insert into produtos (nome, valor, quantidade, descricao) values(?,?,?,?)',
+            array($nome, $valor, $quantidade, $descricao)
+        );
+        //return view('produtos.adicionado')->with('nome', $nome);
+        return redirect('/produtos')->withInput();
+    }
 }
---
+++ resources/views/produtos/formulario.blade.php
@@ -0,0 +1,26 @@
+@extends('layout/principal')
+@section('conteudo')
+
+<form action="/produtos/adiciona" method="post">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div class="form-group">
+        <label>Nome:</label>
+        <input name="nome" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Valor:</label>
+        <input name="valor" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Quantidade:</label>
+        <input name="quantidade" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Descricao:</label>
+        <textarea name="descricao" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+    
+    <button class="btn btn-primary" type="submit">Adicionar</button>
+</form>
+
+@stop
